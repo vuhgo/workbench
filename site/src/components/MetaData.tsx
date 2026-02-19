6b332fef36a70cea1406b82e23e05cb07037a250
hugivar <red.clock0044@fastmail.com>
2026-02-18 22:54:51 -0500

    update deps and cleanup dead code

--- site/src/components/MetaData.tsx
+++ site/src/components/MetaData.tsx
@@ -1,28 +1,17 @@
-import React from "react";
-import { Helmet } from "react-helmet-async";
-
-import settings from "config/settings";
+import Head from "next/head";
 
 interface IMetaData {
   title?: string;
   description?: string;
-  bodyClass?: object;
 }
 
-export const MetaData = ({ title, description, bodyClass }: IMetaData) => {
-  title = title || settings.siteTitleMeta || settings.title;
-  description = description || settings.siteDescriptionMeta;
-
+export const MetaData = ({ title = "Hugivar", description = "" }: IMetaData) => {
   return (
-    <Helmet>
+    <Head>
       <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta property="og:site_name" content={settings.title} />
+      {description && <meta name="description" content={description} />}
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <html lang={settings.lang} />
-      <body className={bodyClass?.toString()} />
-    </Helmet>
+    </Head>
   );
 };
--- site/src/static/collections/docker-setup-1952.md
+++
@@ -1,9 +0,0 @@
-
----
-title: Docker Setup
-description: Quick Links   https://www.swyx.io/running-docker-without-docker-desktop
----
-
-### Quick Links
-https://www.swyx.io/running-docker-without-docker-desktop 
-
